ca613b14d4a4fd02139be3d4e9f575c4330d2fc1
chaitanyayugo <chaitanyayugo@gmail.com>
2026-07-07 15:13:42 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Portfolio",
+  basePath: "Portfolio",
   trailingSlash: true,
   images: { unoptimized: true }
 };
